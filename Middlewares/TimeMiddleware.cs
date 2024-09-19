63a84715f9f56bc431c93debc377638a3954f070
Andres M <marulandaandres19@gmail.com>
2024-09-18 21:18:47 -0500

    middlewares

---
+++ Middlewares/TimeMiddleware.cs
@@ -0,0 +1,30 @@
+public class TimeMiddleware
+{
+
+  readonly RequestDelegate next;
+
+  public TimeMiddleware(RequestDelegate nextRequest)
+  {
+    next = nextRequest;
+  }
+
+  public async Task Invoke(HttpContext context)
+  {
+
+
+    if (context.Request.Query.Any(P => P.Key == "time"))
+    {
+      await context.Response.WriteAsync(DateTime.Now.ToShortTimeString());
+    }
+    await next(context);
+  }
+
+}
+
+public static class TimeMiddlewareExtension
+{
+  public static IApplicationBuilder UseTimeMiddleware(this IApplicationBuilder builder)
+  {
+    return builder.UseMiddleware<TimeMiddleware>();
+  }
+}
--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,16 @@
 # INSTALACIÓN
 
 1. dotnet new webapi -controllers
+
+# Middlewares:
+
+- Es una serie de instrucciones de código que se agregan al cilco de vida de una petición HTTP.
+
+Provee una ejecucion de peticiones a través de capas.
+
+Facilitan la implementación de interceptores y filtros sobre las peticiones en una API.
+
+# Middleware personalizado:
+
+1. En raiz crear una carpeta llamada middlewares
+2. Dentro crear un archivo llamado TimeMiddleware.cs
